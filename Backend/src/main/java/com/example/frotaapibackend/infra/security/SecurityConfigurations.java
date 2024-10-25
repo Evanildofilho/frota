6d8864bad7b358f836c87d84b9204d1a54100c37
Evanildofilho <evanildo_filho@hotmail.com>
2024-10-25 15:53:03 -0300

    Inicializa o repositório com a branch main

--- Backend/src/main/java/com/example/frotaapibackend/infra/security/SecurityConfigurations.java
+++ Backend/src/main/java/com/example/frotaapibackend/infra/security/SecurityConfigurations.java
@@ -27,7 +27,7 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/user").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN")
                         .anyRequest().authenticated()
